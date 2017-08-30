1601453043adef34db3d8b431756852cbc72504c
hoxnox <hoxnox@gmail.com>
2017-08-30 09:35:58 +0300

    Initial commit

---
+++ doc/index.h
@@ -0,0 +1,22 @@
+﻿////////////////////////////////////////////////////////////////////////
+// index.h
+// Author: $username$ <$usermail$>
+////////////////////////////////////////////////////////////////////////
+
+/**
+
+@image html main_logo.png
+@author $username$ <$usermail$>
+@date $date$
+
+@mainpage skyscrappers
+
+@tableofcontents
+
+@section intro Intro
+
+@subsection desc Detail
+
+@li @subpage pagename
+
+*/
