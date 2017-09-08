0fca38d0dad817b6842c25621d129835aa3144af
hoxnox <hoxnox@gmail.com>
2017-09-08 22:07:35 +0300

    ENH: beutify and attempt to optimize

--- SkyScrapers.hpp
+++ SkyScrapers.hpp
@@ -51,6 +51,7 @@ private:
     static uint8_t ridx(uint8_t i) { return (N-1 - i%N) + N*(i/N) + (i<N*2 ? 1 : -1)*2*N; }
 
     std::array<uint8_t, N*4> clues_{};  // current task
+    std::array<uint8_t, 2>   filled_{}; // count of filled cols and rows
     int                      matrix_{}; // current solved matrix index
 
     // pre-generated permutations and visions 0 -> {}, 1 -> {{{3, 2, 1}, 3}, {{3, 1, 2}, 2}}, ...
@@ -64,7 +65,7 @@ private:
     std::array<std::vector<Permutation>, N> permutations_{};
 
     ///@brief replace matrix line with given line by clue index if it's possible
-    static Matrix fill(Matrix matrix, Line line, int idx);
+    static Matrix fill(Matrix m, const uint8_t* iline, bool fwd, uint8_t fixed, bool is_vertical);
     ///@brief get all possible lines for the given clue and try to fill every template
     ///
     /// @param threshold - if the size of the result is greater then threshold - give up
@@ -76,16 +77,71 @@ private:
     std::vector<Matrix> generate(uint8_t i, uint8_t j,
                                  const std::vector<Matrix>& templates);
 
+    ///@brief estimate how much for the given clue will be generated candidates
+    size_t estimate_count(uint8_t idx) const;
+
+    ///@breaf for clue N and 1 the elements are defined
+    ///@return rows and cols fille
+    void fill_simple_clues(Matrix& m);
+
+    ///@brief for every candidate brute zero elements
+    int** brute_windows(const std::vector<Matrix>& candidates);
+
+    ///@brief get indices for non zero and non N clues without counterparts
+    std::list<uint8_t> clue_indices() const;
+
     // storage for answers (4x4 task requires int** as output)
     std::vector<std::shared_ptr<std::array<int*, N>>> storage_;
     std::vector<std::shared_ptr<std::array<int, N>>> row_storage_;
 };
 
 template<uint8_t N>
+int**
+SkyScrapers<N>::Solve(const int* clues)
+{
+    filled_={0, 0};
+    std::transform(clues, clues + N*4, clues_.data(), [](int v){ return (uint8_t)v; });
+
+    Matrix matrix{N};
+    fill_simple_clues(matrix);
+
+    std::list<uint8_t> pending = clue_indices();
+    std::vector<Matrix> candidates{matrix};
+    while (!pending.empty())
+    {
+        pending.sort([this](const uint8_t& lhv, const uint8_t& rhv)
+                     { return estimate_count(lhv) < estimate_count(rhv); });
+        auto best_pending = pending.begin();
+        std::vector<Matrix> best_candidates;
+        size_t min_sz = std::numeric_limits<size_t>::max();
+        for(auto cur = pending.begin(); cur != pending.end(); ++cur)
+        {
+            auto tmp = generate(*cur, candidates, min_sz);
+            LOG(INFO) << "   check " << (int)*cur << "(" << (int)clues_[*cur] << "): " << tmp.size() << std::endl;
+            if (tmp.size() < min_sz && tmp.size() > 0)
+            {
+                best_candidates = tmp;
+                min_sz = tmp.size();
+                best_pending = cur;
+                if (min_sz <= candidates.size()*4)
+                    break;
+            }
+        }
+        candidates = best_candidates;
+        LOG(INFO) << (int)*best_pending << "(" << (int)clues_[*best_pending] << "): " << candidates.size() << std::endl;
+        ++filled_[*best_pending/N%2];
+        pending.erase(best_pending);
+    }
+
+    // at that moment there are only few candidates with some zero elements, let's brute
+    return brute_windows(candidates);
+}
+
+template<uint8_t N>
 SkyScrapers<N>::SkyScrapers()
 {
     if (N > 9)
-        return;
+        throw std::runtime_error("Designed for N<9.");
 
     // put zero element into storage as zero solution
     storage_.emplace_back(new std::array<int*, N>{});
@@ -108,21 +164,19 @@ SkyScrapers<N>::SkyScrapers()
 
 template<uint8_t N>
 inline typename SkyScrapers<N>::Matrix
-SkyScrapers<N>::fill(Matrix m, std::array<uint8_t, N> line, int idx)
+SkyScrapers<N>::fill(Matrix m, const uint8_t* iline, bool fwd, uint8_t fixed, bool is_vertical)
 {
-    uint8_t fixed = (idx<2*N ? idx%N : N-1-idx%N);
-    uint8_t direction = idx/N;
-    if (direction==1 || direction==2)
-        std::reverse(line.begin(), line.end());
-    bool is_vertical = (direction%2==0);
-
     // make attempt to fill line, if it's not possible - return empty matrix
     for (uint8_t i = 0; i < N; ++i)
     {
         uint8_t& v = (is_vertical ? m[i][fixed] : m[fixed][i]);
-        if (v != 0 && v != line[i])
+        if (v != 0 && v != *iline)
             return {};
-        v = line[i];
+        v = *iline;
+        if (fwd)
+            ++iline;
+        else
+            --iline;
     }
 
     // check row and col for duplicates
@@ -152,21 +206,32 @@ SkyScrapers<N>::generate(int i, const std::vector<SkyScrapers<N>::Matrix>& templ
     std::vector<Matrix> rs;
     if (max < std::numeric_limits<size_t>::max())
         rs.reserve(max);
+    uint8_t counter = 0;
     uint8_t rclue = clues_[ridx(i)];
+
+    uint8_t fixed = (i<2*N ? i%N : N-1-i%N);
+    uint8_t direction = i/N;
+    bool is_vertical = (direction%2==0);
+
     for (const auto& tpl : templates)
     {
         for (const auto& line: permutations_[clues_[i]])
         {
             if (rclue == 0 || line.rev_vision == rclue)
             {
-                auto matrix = fill(tpl, line.line, i);
+                auto matrix = (direction==1 || direction==2) ?
+                    fill(tpl, line.line.data() + N-1, false, fixed, is_vertical) :
+                    fill(tpl, line.line.data(), true, fixed, is_vertical);
                 if (!matrix.empty())
+                {
                     rs.emplace_back(std::move(matrix));
+                    ++counter;
+                }
             }
-            if (rs.size() > max)
+            if (counter > max)
                 break;
         }
-        if (rs.size() > max)
+        if (counter > max)
             break;
     }
     rs.shrink_to_fit();
@@ -211,37 +276,31 @@ SkyScrapers<N>::generate(uint8_t i, uint8_t j, const std::vector<SkyScrapers<N>:
 }
 
 template<uint8_t N>
-int**
-SkyScrapers<N>::Solve(const int* clues)
+void
+SkyScrapers<N>::fill_simple_clues(Matrix& matrix)
 {
-    if (N > 9)
-        return nullptr;
-
-    Matrix matrix{N};
-    for (uint8_t i = 0; i < N*4; ++i)
-        clues_[i] = static_cast<uint8_t>(clues[i]);
-
-    uint8_t best_prev = 0;
-
-    uint8_t filled[] = {0, 0};
     // The line for clue = N is determined, we should fill it
-    auto clueN = std::find(clues, clues + 4*N, N);
-    while (clueN != clues + 4*N)
+    auto clueN = std::find(clues_.begin(), clues_.end(), N);
+    while (clueN != clues_.end())
     {
         Line line;
         for (uint8_t i = 0; i < N; ++i)
             line[i] = i + 1;
-        matrix = fill(matrix, line, clueN - clues);
-        best_prev = clueN-clues;
-        ++filled[(clueN-clues)/N%2];
-        clueN = std::find(clueN+1, clues + 4*N, N);
+        uint8_t idx = clueN - clues_.begin();
+        uint8_t fixed = (idx<2*N ? idx%N : N-1-idx%N);
+        uint8_t direction = idx/N;
+        bool is_vertical = (direction%2==0);
+        matrix = (direction == 1 || direction == 2) ?
+              fill(matrix, line.data() + N-1, false, fixed, is_vertical) :
+              fill(matrix, line.data(), true, fixed, is_vertical);        ++filled_[idx/N%2];
+        clueN = std::find(clueN+1, clues_.end(), N);
     }
 
     // matrix cell for clue = 1 is determined, we should fill it too
-    auto clue1_ = std::find(clues, clues + 4*N, 1);
-    if (clue1_ != clues + 4*N)
+    auto clue1_ = std::find(clues_.begin(), clues_.end(), 1);
+    if (clue1_ != clues_.end())
     {
-        auto clue1 = clue1_ - clues;
+        auto clue1 = clue1_ - clues_.begin();
         if (clue1 < N)
             matrix[0][clue1] = N;
         if (clue1 > N && clue1 < 2*N)
@@ -251,92 +310,12 @@ SkyScrapers<N>::Solve(const int* clues)
         if (clue1 > 3*N && clue1 < 4*N)
             matrix[N-1-clue1%N][0] = N;
     }
+}
 
-
-    auto find = [](const std::list<uint8_t>& list, uint8_t val) -> std::list<uint8_t>::const_iterator
-    {
-        auto i = list.begin();
-        for (; i != list.end(); ++i)
-            if (val == *i)
-                break;
-        return i;
-    };
-    std::list<uint8_t> pending;
-    for (uint8_t i = 0; i < clues_.size(); ++i)
-        if (clues_[i] != 0 && clues_[i] != N)
-            pending.emplace_back(i);
-
-    std::vector<Matrix> candidates{matrix};
-    while (!pending.empty())
-    {
-        pending.sort([this, clues, filled](const uint8_t& lhv, const uint8_t& rhv)
-                {
-                    size_t lperm = permutations_[clues[lhv]].size();
-                    size_t rperm = permutations_[clues[rhv]].size();
-
-                    if (clues[ridx(lhv)] != 0)
-                    {
-                        size_t count = 0;
-                        for (const auto& p : permutations_[clues[lhv]])
-                            if (p.rev_vision == clues[ridx(lhv)])
-                                ++count;
-                        lperm = count;
-                    }
-
-                    if (clues[ridx(rhv)] != 0)
-                    {
-                        size_t count = 0;
-                        for (const auto& p : permutations_[clues[rhv]])
-                            if (p.rev_vision == clues[ridx(rhv)])
-                                ++count;
-                        rperm = count;
-                    }
-
-                    size_t lredux = 1;
-                    for (uint8_t i = 0; i < filled[(lhv+1)/N%2]; ++i)
-                        lredux *= N-i;
-
-                    size_t rredux = 1;
-                    for (uint8_t i = 0; i < filled[(rhv+1)/N%2]; ++i)
-                        rredux *= N-i;
-
-                    return lperm/lredux < rperm/rredux;
-                });
-        auto best_pending = pending.begin();
-        std::vector<Matrix> best_candidates;
-        size_t min_sz = std::numeric_limits<size_t>::max();
-        std::list<uint8_t> rskip;
-        for(auto cur = pending.begin(); cur != pending.end(); ++cur)
-        {
-            if (find(rskip, *cur) != rskip.end())
-                continue;
-
-            auto tmp = generate(*cur, candidates, min_sz);
-            LOG(INFO) << "   check " << (int)*cur << ": " << tmp.size() << std::endl;
-            if (clues_[ridx(*cur)] != 0)
-                rskip.emplace_back(ridx(*cur));
-            if (tmp.size() < min_sz && tmp.size() > 0)
-            {
-                best_candidates = tmp;
-                min_sz = tmp.size();
-                best_pending = cur;
-                best_prev = *cur;
-                if (min_sz <= candidates.size()*4)
-                    break;
-            }
-        }
-        candidates = best_candidates;
-        LOG(INFO) << "generated " << (int)*best_pending << "(" << (int)clues_[*best_pending] << "): " << candidates.size() << std::endl;
-        pending.erase(best_pending);
-        ++filled[best_prev/N%2];
-        if (clues_[ridx(best_prev)] != 0)
-        {
-            auto tmp = find(pending, ridx(best_prev));
-            if (tmp != pending.end())
-                pending.erase(tmp);
-        }
-    }
-
+template<uint8_t N>
+int**
+SkyScrapers<N>::brute_windows(const std::vector<Matrix>& candidates)
+{
     if (candidates.size() < 1)
         return nullptr;
 
@@ -367,6 +346,43 @@ SkyScrapers<N>::Solve(const int* clues)
             return (int**)(storage_[matrix_]->data());
         }
     }
-
-    return nullptr;
 }
+
+template<uint8_t N>
+size_t
+SkyScrapers<N>::estimate_count(uint8_t idx) const
+{
+    size_t perms = permutations_[clues_[idx]].size();
+    if (clues_[ridx(idx)] != 0)
+    {
+        size_t count = 0;
+        for (const auto& p : permutations_[clues_[idx]])
+            if (p.rev_vision == clues_[ridx(idx)])
+                ++count;
+        perms = count;
+    }
+
+    size_t redux = 1;
+    for (uint8_t i = 0; i < filled_[(idx+1)/N%2]; ++i)
+        redux *= N-i;
+
+    return perms/redux;
+}
+
+template<uint8_t N>
+std::list<uint8_t>
+SkyScrapers<N>::clue_indices() const
+{
+    std::list<uint8_t> rs;
+    for (uint8_t i = 0; i < clues_.size(); ++i)
+    {
+        if (clues_[i] != 0 && clues_[i] != N)
+        {
+            if (clues_[ridx(i)] != 0 && i >= N*2)
+                continue;
+            rs.emplace_back(i);
+        }
+    }
+    return rs;
+}
+
